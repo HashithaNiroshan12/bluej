3a574099714252854d3b5c98dd13f8804078649b
hashi16 <mahniroshan16@gmail.com>
2021-01-16 10:11:04 +0530

    Initial sharing of project

---
+++ Test.java
@@ -0,0 +1,17 @@
+
+
+ public class Test  {
+
+    public static void main(String[] args){
+        for(int i=0;i<5;i++){
+           test(i);
+        }   
+    }
+    
+    public static void test(int n){
+    
+       System.out.println(n);
+    }
+}
+
+
